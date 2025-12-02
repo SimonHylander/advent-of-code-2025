94c249ffa5a549cb80dcde6a2bc431c72dfad8fd
Simon Hylander <simon.hylander@byggdagboken.se>
2025-12-02 13:51:26 +0100

    day 2

--- src/days/02.ts
+++ src/days/02.ts
@@ -1,4 +1,5 @@
 import { getInput } from "~/input";
+import assert from "assert";
 
 export default {
   partA,
@@ -7,10 +8,43 @@ export default {
 
 export async function partA() {
   const input = await getInput(2);
-  const lines = input.split("\n");
+  const lines = input.split(",");
+
+  const sum = lines.reduce((acc, line) => {
+    const [start, end] = line.split("-").map(Number);
+
+    return (
+      acc +
+      Array.from({ length: end - start + 1 }, (_, i) => start + i)
+        .filter((i) => {
+          const str = i.toString();
+          if (str.length % 2 !== 0) {
+            return false;
+          }
+
+          const half = str.length / 2;
+          return str.slice(0, half) === str.slice(half);
+        })
+        .reduce((acc, id) => acc + id, 0)
+    );
+  }, 0);
+
+  assert(sum === 38437576669);
 }
 
 async function partB() {
   const input = await getInput(2);
-  const lines = input.split("\n");
+  const lines = input.split(",");
+
+  const sum = lines.reduce((acc, line) => {
+    const [start, end] = line.split("-").map(Number);
+    return (
+      acc +
+      Array.from({ length: end - start + 1 }, (_, i) => start + i)
+        .filter((i) => i.toString().match(/^(\d+)\1+$/) !== null)
+        .reduce((acc, id) => acc + id, 0)
+    );
+  }, 0);
+
+  assert(sum === 49046150754);
 }
